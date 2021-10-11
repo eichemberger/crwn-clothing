2b65ae8f4ea59bea93085868a835f902f084cfe0
German Eichemberger <germaneichemberger@gmail.com>
2021-10-11 08:31:40 -0300

    implemented firebase utils, included ability to store authenticated users into firebase

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyDZ7YC33-6FLHALpbn0fXEgfnHcFVbDZn4',
   authDomain: 'crwn-db-65685.firebaseapp.com',
@@ -12,6 +10,32 @@ const firebaseConfig = {
   messagingSenderId: '439061309763',
   appId: '1:439061309763:web:e99a2be3ca5fae3867a7f6',
   measurementId: 'G-QRJJ77KBZ6',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 // Initialize Firebase
