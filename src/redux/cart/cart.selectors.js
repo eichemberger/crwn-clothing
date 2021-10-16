419716ce1bfd478fa9b2569d2c1670c842918373
German Eichemberger <germaneichemberger@gmail.com>
2021-10-16 15:47:19 -0300

    added checkout page and checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -20,3 +20,11 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (acumulatedQuantity, cartItem) =>
+      acumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
