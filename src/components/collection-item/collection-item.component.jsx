3efaf56ea778abe8ff26875dbfddd5d96ffa813d
German Eichemberger <germaneichemberger@gmail.com>
2021-10-19 19:15:32 -0300

    move directory, shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import './collection-item.component.scss';
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
